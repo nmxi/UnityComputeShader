36375caa21b409073c8d9e3f9b5cff6ec19abd46
nmxi <mikki1118_2@hotmail.co.jp>
2018-05-20 19:09:13 +0900

    Update

---
+++ Assets/Scripts/CStestArray.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSTestArray : MonoBehaviour {
+
+    [SerializeField] private ComputeShader _computeShader;
+    int CSFunction_testIndex;
+    ComputeBuffer intComputeBuffer;
+
+	// Use this for initialization
+	void Start () {
+        //カーネルインデックスの保存
+        CSFunction_testIndex = _computeShader.FindKernel("CSFunction_test");
+
+        intComputeBuffer = new ComputeBuffer(6, sizeof(int));   //ComputeShaderの初期化
+
+        //ComputeShaderにどのカーネルバッファを設定するのか，カーネルバッファの名前の設定
+        //C#側で使うComputeBufferの指定，これを再び定義してあげることで
+        //同一ComputeShader内の別カーネルの計算をComputeShader側の変数を
+        //共有のまま計算命令できる ****ここ結構重要****
+        _computeShader.SetBuffer(CSFunction_testIndex, "intBuffer", intComputeBuffer);
+
+        _computeShader.SetInt("intValue", 1);   //ComputeShaderに値を渡す
+        _computeShader.Dispatch(CSFunction_testIndex, 1, 1, 1);  //ComputeShaderの実行
+
+        int[] result = new int[6];  //ComputeShaderの結果受け取り用
+        intComputeBuffer.GetData(result);   //結果の取り出し
+
+        for (int i = 0; i < 6; i++) {
+            Debug.Log(result[i]);   //結果を出力
+        }
+
+        intComputeBuffer.Release();
+	}
+}
---
+++ Assets/Scripts/CStestTexture.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CStestTexture : MonoBehaviour {
+
+    [SerializeField] private ComputeShader _computeShaderTextureTest;
+    [SerializeField] private GameObject _plane;
+    private RenderTexture _renderTexture;
+    private Renderer _renderer;
+    private int _kernelIndex_PanelColor;
+
+    struct ThreadSize {
+        public int x;
+        public int y;
+        public int z;
+
+        public ThreadSize(uint x, uint y, uint z) {
+            this.x = (int)x;
+            this.y = (int)y;
+            this.z = (int)z;
+        }
+    }
+
+    private ThreadSize _kernelThreadSize_PanelColor;
+
+    // Use this for initialization
+    void Start () {
+        //Initialized RenderTexture
+        _renderTexture = new RenderTexture(512, 512, 0, RenderTextureFormat.ARGB32);
+
+        _renderTexture.enableRandomWrite = true;
+        _renderTexture.Create();
+
+        //ComputeShader settings
+        _kernelIndex_PanelColor = _computeShaderTextureTest.FindKernel("PanelColor");
+
+        uint threadSizeX;
+        uint threadSizeY;
+        uint threadSizeZ;
+
+        _computeShaderTextureTest.GetKernelThreadGroupSizes
+            (_kernelIndex_PanelColor, out threadSizeX, out threadSizeY, out threadSizeZ);
+
+        _kernelThreadSize_PanelColor = new ThreadSize(threadSizeX, threadSizeY, threadSizeZ);
+
+        //Output Setting
+        _computeShaderTextureTest.SetTexture
+            (_kernelIndex_PanelColor, "Result", _renderTexture);
+
+        _renderer = _plane.GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        _computeShaderTextureTest.SetFloats("Time", Time.frameCount);   //Send frame time for computeShader
+        _computeShaderTextureTest.Dispatch(_kernelIndex_PanelColor,
+                                           _renderTexture.width / _kernelThreadSize_PanelColor.x,
+                                           _renderTexture.height / _kernelThreadSize_PanelColor.y,
+                                           _kernelThreadSize_PanelColor.z);
+
+        _renderer.material.mainTexture = _renderTexture;
+	}
+}
